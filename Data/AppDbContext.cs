50c5271f1e3ccfc7f3b4df8dfc6ad0b6cc8d852e
GervinAnsi <158846288+GervinAnsi@users.noreply.github.com>
2025-06-09 23:35:28 +0300

    create customer view jne

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -11,5 +11,7 @@
         }
         public virtual DbSet<City> Cities { get; set; }
         public virtual DbSet<Country> Countries { get; set; }
+
+        public virtual DbSet<Customer> Customer { get; set; }
     }
 }
